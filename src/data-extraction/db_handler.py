74eda9d725044d67299ae4c777f7339e0bda6505
Hriday Baghar <hbaghar@uw.edu>
2022-02-27 02:41:29 -0800

    Updated table def, defined some insert functions

--- src/data-extraction/db_handler.py
+++ src/data-extraction/db_handler.py
@@ -1,4 +1,5 @@
 import sqlite3 as sql
+from typing import final
 import fpl_api_handler as fpl
 
 class DBHandler():
@@ -23,14 +24,14 @@ class DBHandler():
                                                 ep_next DOUBLE, event_points INTEGER, form DOUBLE , goals_conceded INTEGER, goals_scored INTEGER,
                                                 ict_index DOUBLE, ict_index_rank INTEGER, ict_index_rank_type INTEGER,
                                                 influence DOUBLE, influence_rank INTEGER, influence_rank_type INTEGER, minutes INTEGER,
-                                                news TEXT, news_added DATETIME, now_cost INTEGER, own_goals INTEGER,
+                                                news TEXT, news_added TEXT, now_cost INTEGER, own_goals INTEGER,
                                                 penalties_missed INTEGER, penalties_order INTEGER, penalties_saved INTEGER, penalties_text TEXT, photo TEXT,
                                                 points_per_game INTEGER, red_cards INTEGER, saves INTEGER, selected_by_percent DOUBLE, 
                                                 threat DOUBLE, threat_rank INTEGER, threat_rank_type INTEGER, total_points INTEGER,
                                                 value_form DOUBLE, value_season DOUBLE, yellow_cards INTEGER,
                                                 FOREIGN KEY(team) REFERENCES teams(id), FOREIGN KEY(element_type) REFERENCES positions(id));
         CREATE TABLE IF NOT EXISTS metric_names_static (name TEXT PRIMARY KEY, label TEXT);
-        CREATE TABLE IF NOT EXISTS events_static (id INTEGER PRIMARY KEY, average_entry_score INTEGER, data_checked, deadline_time TEXT, finished TEXT, highest_score INTEGER, 
+        CREATE TABLE IF NOT EXISTS events_static (id INTEGER PRIMARY KEY, average_entry_score INTEGER, data_checked TEXT, deadline_time TEXT, finished TEXT, highest_score INTEGER, 
                                                     is_current TEXT, is_next TEXT, is_previous TEXT, most_captained INTEGER, most_selected INTEGER, 
                                                     most_transferred_in INTEGER, most_vice_captained INTEGER,
                                                     name TEXT, top_element INTEGER, top_element_info_id INTEGER, top_element_info_points INTEGER, transfers_made INTEGER);
@@ -44,8 +45,44 @@ class DBHandler():
                                                         PRIMARY KEY (element, round), FOREIGN KEY(element) REFERENCES players(id), FOREIGN KEY(fixture) REFERENCES fixtures(id));"""
         self.cursor.executescript(q)
         self.conn.commit()
+    
+    def static_inserts(self):
+        """
+        Inserts static data into the database - data that is not updated every week
+        """
+        teams_static = fpl.get_static_data("teams")
+        positions_static = fpl.get_static_data("element_types")
+        metric_names_static = fpl.get_static_data("element_stats")
 
+        with self.conn:
+            try:
+                self.conn.executemany("INSERT INTO teams_static VALUES (:id, :name, :code, :short_name)", teams_static)
+                self.conn.executemany("INSERT INTO positions_static VALUES (:id, :singular_name_short, :squad_max_play, :squad_min_play)", positions_static)
+                self.conn.executemany("INSERT INTO metric_names_static VALUES (:name, :label)", metric_names_static)
+            except sql.IntegrityError:
+                print("Static data already inserted")
+
+    def update_events(self):
+        """
+        Inserts events into the database
+        """
+        events = fpl.get_static_data("events")
+        for event in events:
+            event["top_element_info_id"] = event["top_element_info"]["id"]
+            event["top_element_info_points"] = event["top_element_info"]["points"]
+            event.pop("top_element_info")
+        with self.conn:
+            try:
+                self.conn.executemany("""INSERT INTO events_static VALUES (:id, :average_entry_score, :data_checked, :deadline_time, :finished, 
+                                                                        :highest_score, :is_current, :is_next, :is_previous, :most_captained, 
+                                                                        :most_selected, :most_transferred_in, :most_vice_captained, :name, :top_element, 
+                                                                        :top_element_info_id, :top_element_info_points, :transfers_made)""", events)
+            except sql.IntegrityError:
+                print("Event already inserted")
+        
 if __name__ == "__main__":
     db = DBHandler()
     db.create_default_tables()
+    db.static_inserts()
+    db.update_events()
     db.conn.close()
