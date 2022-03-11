58eeef70fb079136c87ad69b8881cb2e81db95ce
Hriday Baghar <hbaghar@uw.edu>
2022-03-11 04:00:10 -0800

    Shifted db creation code and sped up player gw updates

--- src/data-extraction/db_handler.py
+++ src/data-extraction/db_handler.py
@@ -1,6 +1,9 @@
+from functools import partial
 import sqlite3 as sql
 from tqdm import tqdm
+from itertools import repeat
 import fpl_api_handler as fpl
+from multiprocessing import Pool
 class DBHandler():
     """
     Class with methods to create a database and perform operations on it exclusively related to FPL API
@@ -14,48 +17,16 @@ class DBHandler():
         self.static_inserts()
 
     def create_default_tables(self):
-        q = """  
-        CREATE TABLE IF NOT EXISTS teams_static (id INTEGER PRIMARY KEY, name TEXT, code INTEGER, short_name TEXT);
-        CREATE TABLE IF NOT EXISTS positions_static (id INTEGER PRIMARY KEY, singular_name_short TEXT, squad_max_play INTEGER, squad_min_play INTEGER);
-        CREATE TABLE IF NOT EXISTS players_static (id INTEGER PRIMARY KEY, first_name TEXT, second_name TEXT, web_name TEXT, team INTEGER, element_type INTEGER, 
-                                                assists INTEGER, bonus INTEGER, bps INTEGER, chance_of_playing_next_round INTEGER, chance_of_playing_this_round INTEGER,
-                                                clean_sheets INTEGER, code INTEGER, corners_and_indirect_freekicks_order INTEGER, corners_and_indirect_freekicks_text TEXT,
-                                                cost_change_event INTEGER, cost_change_start INTEGER, creativity DOUBLE, creativity_rank INTEGER, creativity_rank_type INTEGER,
-                                                direct_freekicks_order INTEGER, direct_freekicks_text TEXT, dreamteam_count INTEGER,
-                                                ep_next DOUBLE, event_points INTEGER, form DOUBLE , goals_conceded INTEGER, goals_scored INTEGER,
-                                                ict_index DOUBLE, ict_index_rank INTEGER, ict_index_rank_type INTEGER,
-                                                influence DOUBLE, influence_rank INTEGER, influence_rank_type INTEGER, minutes INTEGER,
-                                                news TEXT, news_added TEXT, now_cost INTEGER, own_goals INTEGER,
-                                                penalties_missed INTEGER, penalties_order INTEGER, penalties_saved INTEGER, penalties_text TEXT, photo TEXT,
-                                                points_per_game INTEGER, red_cards INTEGER, saves INTEGER, selected_by_percent DOUBLE, 
-                                                threat DOUBLE, threat_rank INTEGER, threat_rank_type INTEGER, total_points INTEGER,
-                                                value_form DOUBLE, value_season DOUBLE, yellow_cards INTEGER,
-                                                FOREIGN KEY(team) REFERENCES teams(id), FOREIGN KEY(element_type) REFERENCES positions(id));
+        with open("src/data-extraction/create_tables.sql", "r") as f:
+            self.cursor.executescript(f.read())
         
-        CREATE TABLE IF NOT EXISTS metric_names_static (name TEXT PRIMARY KEY, label TEXT);
-        
-        CREATE TABLE IF NOT EXISTS events_static (id INTEGER PRIMARY KEY, average_entry_score INTEGER, data_checked TEXT, deadline_time TEXT, finished TEXT, highest_score INTEGER, 
-                                                    is_current TEXT, is_next TEXT, is_previous TEXT, most_captained INTEGER, most_selected INTEGER, 
-                                                    most_transferred_in INTEGER, most_vice_captained INTEGER,
-                                                    name TEXT, top_element INTEGER, top_element_info_id INTEGER, top_element_info_points INTEGER, transfers_made INTEGER);
-        
-        CREATE TABLE IF NOT EXISTS fixtures (id INTEGER PRIMARY KEY, event INTEGER, team_a INTEGER, team_h INTEGER, team_a_difficulty INTEGER, team_h_difficulty INTEGER);
-        
-        CREATE TABLE IF NOT EXISTS player_gw_detailed (element INTEGER, assists INTEGER, bonus INTEGER, bps INTEGER, clean_sheets INTEGER, creativity DOUBLE, fixture INTEGER, 
-                                                        goals_conceded INTEGER, goals_scored INTEGER, ict_index DOUBLE, influence DOUBLE, kickoff_time TEXT, 
-                                                        minutes INTEGER, opponent_team INTEGER, own_goals INTEGER, penalties_missed INTEGER, penalties_saved INTEGER, 
-                                                        red_cards INTEGER, round INTEGER, saves INTEGER, selected INTEGER, team_a_score INTEGER, 
-                                                        team_h_score INTEGER, threat DOUBLE, total_points INTEGER, transfers_balance INTEGER, transfers_in INTEGER, 
-                                                        transfers_out INTEGER, value INTEGER, was_home TEXT, yellow_cards INTEGER,
-                                                        CONSTRAINT player_week_fixture PRIMARY KEY (element, round, fixture), FOREIGN KEY(element) REFERENCES players(id), FOREIGN KEY(fixture) REFERENCES fixtures(id));"""
-        
-        self.cursor.executescript(q)
         self.conn.commit()
     
     def get_table_columns(self, table_name):
         """
         Returns a list of all columns in a table
         """
+
         self.cursor.execute(f"PRAGMA table_info({table_name})")
         return [row[1] for row in self.cursor.fetchall()]
 
@@ -63,6 +34,7 @@ class DBHandler():
         """
         Returns a query string to update or insert rows in a table
         """
+
         columns = self.get_table_columns(table_name)
         insert_query = f"INSERT INTO {table_name} VALUES (:{', :'.join(columns)})"
         for key in pk:
@@ -75,8 +47,8 @@ class DBHandler():
     def static_inserts(self):
         """
         Inserts static data into the database - data that is not updated every week
-        Excepttion added for: teams_static table, it will be updated once a season
         """
+
         teams_static = fpl.get_static_data("teams")
         positions_static = fpl.get_static_data("element_types")
         metric_names_static = fpl.get_static_data("element_stats")
@@ -88,8 +60,6 @@ class DBHandler():
                 self.conn.executemany("INSERT INTO metric_names_static VALUES (:name, :label)", metric_names_static)
             except sql.IntegrityError:
                 print("Static data already inserted")
-                self.conn.rollback()
-                self.conn.executemany("UPDATE teams_static SET name = :name, code = :code, short_name = :short_name WHERE id = :id", teams_static)
     
     def update_events(self):
         """
@@ -103,7 +73,7 @@ class DBHandler():
             if self.cursor.fetchone()[0] == 0:
                 pass
             else:
-                events = [e for e in events if e["is_current"] == 1]
+                events = [e for e in events if e["is_current"] == 1 or e["is_previous"] == 1]
 
             for event in events:
                 event["top_element_info_id"] = event["top_element_info"]["id"]
@@ -118,7 +88,7 @@ class DBHandler():
             try:
                 self.conn.executemany(insert_query, events)
             except sql.IntegrityError:
-                print("Event already inserted, performing update")
+                print("Events already inserted, performing update")
                 self.conn.rollback()
                 self.conn.executemany(update_query, events)
 
@@ -166,35 +136,48 @@ class DBHandler():
                 self.conn.executemany(update_query, players)
     
     def update_player_gw_detailed(self):
-        
-        self.cursor.execute("SELECT id from events_static WHERE is_current = 1")
+        """
+        Updates player_gw_detailed table with player data for each gameweek
+        """
+
+        self.cursor.execute("SELECT MAX(id) from events_static WHERE is_current = 1")
         current_gw = self.cursor.fetchone()[0]
 
         self.cursor.execute("SELECT COUNT(*) FROM player_gw_detailed")
         table_records = self.cursor.fetchone()[0]
         self.cursor.execute("SELECT id FROM players_static")
         
-        for player in tqdm(self.cursor.fetchall()):
-            history = fpl.get_player_info(player[0], "history")
-            insert_query, update_query = self.get_update_and_insert_query("player_gw_detailed", ["element", "round", "fixture"])
-            if table_records == 0:
-                with self.conn:
+        players = self.cursor.fetchall()
+        players = [p[0] for p in players]
+
+        insert_query, update_query = self.get_update_and_insert_query("player_gw_detailed", ["element", "round", "fixture"])
+
+        history = []
+        with Pool(8) as p:
+            with tqdm(total=len(players)) as pbar:
+                for i, data in enumerate(p.imap(partial(fpl.get_player_info, key="history"), players)) :
+                    history.append(data)
+                    pbar.update()
+
+        if table_records == 0:
+            with self.conn:
+                for player in history:
                     try:
-                        self.conn.executemany(insert_query, history)
+                        self.conn.executemany(insert_query, player)
                     except:
-                        print("Player id: ", player[0])
+                        print("Error inserting player", player[0]["element"])
                         raise Exception
-            else:
-                #Performing update for latest gw if player has already been inserted
-                history = [h for h in history if h["round"] == current_gw]
-                with self.conn:
-                    try:
-                        self.conn.executemany(insert_query, history)
-                    except sql.IntegrityError:
-                        self.conn.executemany(update_query, history)
-                    except:
-                        print("Player id: ", player[0])
-                        raise Exception
+        else:
+            #Inserting only current gw records
+            history = [gw for player in history for gw in player if gw["round"] == current_gw]
+            with self.conn:
+                try:
+                    self.conn.executemany(insert_query, history)
+                except sql.IntegrityError:
+                    #Update existing records, if already inserted
+                    self.conn.executemany(update_query, history)
+                except:
+                    raise Exception
             
 
 if __name__ == "__main__":
