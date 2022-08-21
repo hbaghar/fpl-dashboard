4ecc58816e06a9f29534c714144b78c6b5ce2e10
Hriday Baghar <hriday.baghar@gmail.com>
2022-08-21 16:35:08 -0500

    Solved import issues

---
+++ src/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="fpl_dashboard",
+    version="0.0.1",
+    author="Hriday Baghar",
+    packages=setuptools.find_packages(),
+    classifiers=[],
+)
