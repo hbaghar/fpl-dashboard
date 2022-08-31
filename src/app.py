05898fca3b4c58c52cbb7b6c9307450841c17b87
Hriday Baghar <hbaghar@uw.edu>
2022-08-31 00:31:01 -0700

    Added callback filter for position

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ import pandas as pd
 from yaml import safe_load
 from src.data_extraction import db_handler as dbh
 from src.utils import data_table_module as dtm
+from dash.dependencies import Input, Output
 
 db = dbh.DBHandler()
 
@@ -41,29 +42,80 @@ styles_fixtures = dtm.discrete_background_color_bins(
 
 app.layout = html.Div(
     [
-        dt.DataTable(
-            df.to_dict("records"),
-            page_size=20,
-            style_table={
-                "overflowX": "auto",
-                "minWidth": "100%",
-                "padding": "15px",
+        dbc.Row(
+            [html.H1("Fantasy Premier League: Player Tabular Statistics")],
+            style={
+                "text-align": "center",
             },
-            sort_action="native",
-            sort_mode="single",
-            sort_by=[{"column_id": "total_points", "direction": "desc"}],
-            fixed_columns={"headers": True, "data": 3},
-            style_cell={"minWidth": "150px", "maxWidth": "180px"},
-            style_data_conditional=styles_fixtures,
-            style_header={"backgroundColor": "#f1f6ff", "fontWeight": "bold"},
-            columns=[
-                {"name": config["column_names"][i], "id": i}
-                for i in config["column_names"].keys()
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    dt.DataTable(
+                        df.to_dict("records"),
+                        id="player_table",
+                        page_size=20,
+                        style_table={
+                            "overflowX": "auto",
+                            "minWidth": "100%",
+                        },
+                        sort_action="native",
+                        sort_mode="single",
+                        sort_by=[{"column_id": "total_points", "direction": "desc"}],
+                        fixed_columns={"headers": True, "data": 3},
+                        style_cell={"minWidth": "150px", "maxWidth": "180px"},
+                        style_data_conditional=styles_fixtures,
+                        style_header={
+                            "backgroundColor": "#f1f6ff",
+                            "fontWeight": "bold",
+                        },
+                        columns=[
+                            {
+                                "name": config["column_names"][i],
+                                "id": i,
+                                "deletable": True,
+                            }
+                            for i in config["column_names"].keys()
+                        ],
+                        css=[{"selector": ".show-hide", "rule": "display: none"}],
+                    ),
+                    width=10,
+                    style={"border": "1px solid #f1f6ff", "padding": "5px"},
+                ),
+                dbc.Col(
+                    [
+                        html.H5("Position"),
+                        dcc.Dropdown(
+                            id="position_dropdown",
+                            options=config["position_options"],
+                            value="All",
+                            clearable=False,
+                        ),
+                    ],
+                    width=2,
+                    style={"border": "1px solid #f1f6ff", "padding": "5px"},
+                ),
             ],
-            hidden_columns=config["all_players_hidden_columns"],
+            style={"padding": "10px"},
         ),
     ]
 )
+
+
+@app.callback(
+    Output("player_table", "data"),
+    Output("player_table", "hidden_columns"),
+    Input("position_dropdown", "value"),
+)
+def update_player_table(position):
+    if position == "All":
+        return df.to_dict("records"), config["all_players_hidden_columns"]
+    else:
+        hide_cols = (
+            config["all_players_hidden_columns"]
+            + config[position.lower() + "_hidden_columns"]
+        )
+        return df[df["position"] == position].to_dict("records"), hide_cols
 
 
 if __name__ == "__main__":
