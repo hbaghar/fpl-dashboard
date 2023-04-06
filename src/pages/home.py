09b37f304f3f7522ae5f545cd32b8dadbeadeba0
Hriday Baghar <hbaghar@uw.edu>
2023-04-06 01:04:06 -0700

    Updated layout to view top players by gw and season

--- src/pages/home.py
+++ src/pages/home.py
@@ -11,7 +11,27 @@ dash.register_page(__name__, name="Home", path="/")
 layout = html.Div(
     [
         html.H2(id="current-gw", style={"text-align": "center"}),
-        dcc.Graph(id="top5-players"),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [html.H5("View top performers by", style={"text-align": "center"})],
+                    width=2,
+                    style={"margin": "5px -40px 0 5px"},
+                ),
+                dbc.Col(
+                    [
+                        dcc.Dropdown(
+                            id="top-performers-dropdown",
+                            options=["Gameweek", "Season"],
+                            value="Gameweek",
+                            clearable=False,
+                        ),
+                    ],
+                    width=2,
+                ),
+                dcc.Graph(id="top-performers-graph"),
+            ]
+        ),
     ],
 )
 
@@ -25,33 +45,39 @@ def update_current_gw(placeholder):
     return f"Current Gameweek:{current_gw}"
 
 
-@dash.callback(Output("top5-players", "figure"), Input("current-gw", "n_clicks"))
-def top5_players(placeholder):
+@dash.callback(
+    Output("top-performers-graph", "figure"), Input("top-performers-dropdown", "value")
+)
+def top_players_graph(value):
     db = dbh.DBHandler()
+    if value == "Gameweek":
+        score_var = "event_points"
+    elif value == "Season":
+        score_var = "total_points"
+
     current_gw = pd.read_sql_query("SELECT * FROM current_gw_view", db.conn).iloc[0, 0]
     df = pd.read_sql_query(
         f"""
-                            SELECT id, web_name, position, team_name, total_points
-                            FROM  (
-                                SELECT 
-                                    id, web_name, total_points, team_name, position,
-                                    rank() OVER (PARTITION BY position ORDER BY total_points DESC) AS rank
-                                FROM 
-                                    player_window_metrics_view
-                                WHERE 
-                                    round = {current_gw})
-                            WHERE 
-                                rank <= 5
-                            ORDER BY
-                                rank
-                           """,
+            SELECT id, web_name, position, team_name, {score_var}
+            FROM  (
+                SELECT 
+                    id, web_name, position, team_name, {score_var},
+                    rank() OVER (PARTITION BY position ORDER BY {score_var} DESC) AS rank
+                FROM 
+                    player_tabular_view
+                )
+            WHERE 
+                rank <= 5
+            ORDER BY
+                rank
+        """,
         db.conn,
     )
     db.conn.close()
     fig = px.bar(
-        data_frame=df.sort_values(by=["total_points"], ascending=True),
+        data_frame=df.sort_values(by=score_var, ascending=True),
         y="web_name",
-        x="total_points",
+        x=score_var,
         facet_col="position",
         facet_col_wrap=2,
         facet_col_spacing=0.1,
@@ -63,7 +89,7 @@ def top5_players(placeholder):
             "total_points": "Round Points",
             "team_name": "Team",
         },
-        title="Top performers this gameweek",
+        title=f"Top performers this {value.lower()}",
         height=500,
         category_orders={"position": ["GKP", "DEF", "MID", "FWD"]},
     )
