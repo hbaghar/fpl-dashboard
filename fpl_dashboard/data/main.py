e8c4ce2f2ecfde4cb63777620784539402377c57
Hriday Baghar <hriday@dragonfruit.ai>
2024-08-24 01:49:21 -0700

    discard main

--- fpl_dashboard/data/main.py
+++
@@ -1,9 +0,0 @@
-
-import typing
-from tqdm import tqdm
-from fpl_dashboard.data.database_model import *
-from fpl_dashboard.data.fpl_api_handler import *
-
-if typing.TYPE_CHECKING:
-    from .database_model import *
-    from .fpl_api_handler import *
