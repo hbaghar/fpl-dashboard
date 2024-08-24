4b72097cca33e1532ab22674ff40027e12b64b71
Hriday Baghar <hriday@dragonfruit.ai>
2024-08-23 23:35:12 -0700

    imporved api handling with stronger typing and enums

--- src/data_extraction/fpl_api_handler.py
+++ src/data_extraction/fpl_api_handler.py
@@ -1,94 +1,109 @@
 import requests
+from enum import Enum
+from pydantic_models import *
 
-def get_json_data(url):
-    """
-    Get data from the FPL API.
-    """
-    response = requests.get(url)
-    return response.json()
+class FPLAPIEndpoint(Enum):
+    BOOTSTRAP_STATIC = "bootstrap-static/"
+    FIXTURES = "fixtures/"
+    ELEMENT_SUMMARY = "element-summary/"
+    ENTRY = "entry/"
+    PICKS = "entry/{manager_id}/event/{gw}/picks/"
 
-def get_static_data(key):
-    """
-    Get requested data key from bootstrap-static endpoint
+class FPLAPIHandler:
+    def __init__(self, base_url="https://fantasy.premierleague.com/api/"):
+        self.base_url = base_url
 
-    Useful keys:
-    - teams
-    - events (GW overview for FPL)
-    - elements (players)
-    - element_types (player types)
-    - elements_stats (map of player stats labels and column names)
-    """
-    URL = "https://fantasy.premierleague.com/api/bootstrap-static/"
-    data = get_json_data(URL)
+    def _make_request(self, method, endpoint, **kwargs):
+        url = self.base_url + endpoint
+        response = requests.request(method, url, **kwargs)
+        return response.json()
+
+    def get_json_data(self, endpoint, **kwargs):
+        return self._make_request("GET", endpoint, **kwargs)
+
+    def post_json_data(self, endpoint, data, **kwargs):
+        return self._make_request("POST", endpoint, json=data, **kwargs)
+
+    def get_return_type_for_static_data(self, key):
+        if key == "events":
+            return Event
+        elif key == "teams":
+            return Team
+        elif key == "elements":
+            return Element
+        elif key == "element_types":
+            return ElementType
+        elif key == "element_stats":
+            return ElementStat
+        else:
+            raise ValueError(f"Invalid key: {key}")
     
-    #Keeping data till current GW (might have to change to get data only for current GW)
-    if key == "events":
-        data["events"] = [event for event in data["events"] if event["is_current"] == True or event["finished"] == True]
+    def get_static_data(self, key, **kwargs):
+        data = self.get_json_data(FPLAPIEndpoint.BOOTSTRAP_STATIC.value, **kwargs)
+        return_type = self.get_return_type_for_static_data(key)
+        return [return_type.model_validate(item) for item in data[key]]
 
-    return data[key]
+    def get_fixtures(self, **kwargs):
+        data = self.get_json_data(FPLAPIEndpoint.FIXTURES.value, **kwargs)
+        return [Fixture.model_validate(fixture) for fixture in data]
 
-def get_fixtures():
-    """
-    Get all upcoming fixtures from the fixtures endpoint
-    """
-    URL = "https://fantasy.premierleague.com/api/fixtures/"
-    data = get_json_data(URL)
+    def get_player_info(self, element_id, key, **kwargs):
+        endpoint = FPLAPIEndpoint.ELEMENT_SUMMARY.value + f"{element_id}/"
+        data = self.get_json_data(endpoint, **kwargs)[key]
+        if key == "history":
+            return [PlayerHistory.model_validate(item) for item in data]
+        elif key == "fixtures":
+            return [PlayerFixture.model_validate(item) for item in data]
 
-    data = [fixture for fixture in data if fixture['event'] != None]
+    def get_manager_squad(self, manager_id, gw, **kwargs):
+        endpoint = FPLAPIEndpoint.PICKS.value.format(manager_id=manager_id, gw=gw)
+        data = self.get_json_data(endpoint, **kwargs)["picks"]
+        return [ManagerPick.model_validate(item) for item in data]
 
-    for fixture in data:
-        keys = fixture.keys()
-        keys_to_remove = set(keys) - set(["team_h", "team_a", "event", "id", "team_h_difficulty", "team_a_difficulty"])
-        for key in keys_to_remove:
-            fixture.pop(key)
-
-    return data
-
-def get_player_info(element_id, key):
-    """
-    Get player history from the element-summary endpoint
-
-    Possible keys:
-    - history
-    - fixtures
-    """
-    URL = f"https://fantasy.premierleague.com/api/element-summary/{element_id}/"
-    data = get_json_data(URL)
-
-    data = data[key]
-
-    return data
-
-def get_manager_squad(manager_id, gw):
-    """
-    Get manager squad for a given gameweek
-    """
-    URL = f"https://fantasy.premierleague.com/api/entry/{manager_id}/event/{gw}/picks/"
-    data = get_json_data(URL)
-    data = data["picks"]
-    return data
-
-def get_manager_info(manager_id):
-    """
-    Get info for a praticular manager from the entry endpoint
-    """
-    URL = f"https://fantasy.premierleague.com/api/entry/{manager_id}/"
-    data = get_json_data(URL)
-    data.pop("leagues")
-    
-    return data
+    def get_manager_info(self, manager_id, **kwargs):
+        endpoint = FPLAPIEndpoint.ENTRY.value + f"{manager_id}/"
+        data = self.get_json_data(endpoint, **kwargs)
+        return ManagerInfo.model_validate(data)
 
 if __name__ == "__main__":
-    #Running tests
-    import json
-    print(json.dumps(get_static_data("events")[1], indent=4, sort_keys=True))
-    print(json.dumps(get_static_data("teams")[1], indent=4, sort_keys=True))
-    print(json.dumps(get_static_data("elements")[1], indent=4, sort_keys=True))
-    print(json.dumps(get_static_data("element_types")[1], indent=4, sort_keys=True))
-    print(json.dumps(get_static_data("element_stats")[1], indent=4, sort_keys=True))
-    print(json.dumps(get_fixtures()[1], indent=4, sort_keys=True))
-    print(json.dumps(get_player_info(1, "history")[1], indent=4, sort_keys=True))
-    print(json.dumps(get_player_info(1, "fixtures")[1], indent=4, sort_keys=True))
-    print(json.dumps(get_manager_squad(1, 1)[1], indent=4, sort_keys=True))
-    print(json.dumps(get_manager_info(1), indent=4, sort_keys=True))
-    
+    fpl_handler = FPLAPIHandler()
+    # print("Getting static data...")
+    # print("\tGetting events...")
+    # static_data = fpl_handler.get_static_data("events")
+    # print("Static data:", static_data)
+
+    # print("\tGetting teams...")
+    # static_data = fpl_handler.get_static_data("teams")
+    # print("Static data:", static_data)
+
+    print("\tGetting players...")
+    static_data = fpl_handler.get_static_data("elements")
+    print("Static data:", static_data)
+
+    # print("\tGetting player types...")
+    # static_data = fpl_handler.get_static_data("element_types")
+    # print("Static data:", static_data)
+
+    # print("\tGetting element stats...")
+    # static_data = fpl_handler.get_static_data("element_stats")
+    # print("Static data:", static_data)
+
+    # print("Getting fixtures...")
+    # fixtures = fpl_handler.get_fixtures()
+    # print("Fixtures:", fixtures)
+
+    # print("Getting player history info...")
+    # player_info = fpl_handler.get_player_info(1, "history")
+    # print("Player info:", player_info)
+
+    # print("Getting player fixture info...")
+    # player_fixture_info = fpl_handler.get_player_info(1, "fixtures")
+    # print("Player fixture info:", player_fixture_info)
+
+    # print("Getting manager squad...")
+    # manager_squad = fpl_handler.get_manager_squad(1, 1)
+    # print("Manager squad:", manager_squad)
+
+    # print("Getting manager info...")
+    # manager_info = fpl_handler.get_manager_info(1)
+    # print("Manager info:", manager_info)
---
+++ src/data_extraction/pydantic_models.py
@@ -0,0 +1,272 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+class Event(BaseModel):
+    id: int
+    name: str
+    deadline_time: str
+    average_entry_score: int
+    finished: bool
+    data_checked: bool
+    highest_scoring_entry: Optional[int]
+    deadline_time_epoch: int
+    deadline_time_game_offset: int
+    highest_score: Optional[int]
+    is_previous: bool
+    is_current: bool
+    is_next: bool
+    chip_plays: List[dict]
+    most_selected: Optional[int]
+    most_transferred_in: Optional[int]
+    top_element: Optional[int]
+    top_element_info: Optional[dict]
+    transfers_made: int
+    most_captained: Optional[int]
+    most_vice_captained: Optional[int]
+
+class Team(BaseModel):
+    code: int
+    draw: int
+    form: Optional[str]
+    id: int
+    loss: int
+    name: str
+    played: int
+    points: int
+    position: int
+    short_name: str
+    strength: int
+    team_division: Optional[str]
+    unavailable: bool
+    win: int
+    strength_overall_home: int
+    strength_overall_away: int
+    strength_attack_home: int
+    strength_attack_away: int
+    strength_defence_home: int
+    strength_defence_away: int
+    pulse_id: int
+
+class Element(BaseModel):
+    chance_of_playing_next_round: Optional[int]
+    chance_of_playing_this_round: Optional[int]
+    code: int
+    cost_change_event: int
+    cost_change_event_fall: int
+    cost_change_start: int
+    cost_change_start_fall: int
+    dreamteam_count: int
+    element_type: int
+    ep_next: str
+    ep_this: str
+    event_points: int
+    first_name: str
+    form: str
+    id: int
+    in_dreamteam: bool
+    news: str
+    news_added: Optional[str]
+    now_cost: int
+    photo: str
+    points_per_game: str
+    second_name: str
+    selected_by_percent: str
+    special: bool
+    squad_number: Optional[int]
+    status: str
+    team: int
+    team_code: int
+    total_points: int
+    transfers_in: int
+    transfers_in_event: int
+    transfers_out: int
+    transfers_out_event: int
+    value_form: str
+    value_season: str
+    web_name: str
+    minutes: int
+    goals_scored: int
+    assists: int
+    clean_sheets: int
+    goals_conceded: int
+    own_goals: int
+    penalties_saved: int
+    penalties_missed: int
+    yellow_cards: int
+    red_cards: int
+    saves: int
+    bonus: int
+    bps: int
+    influence: str
+    creativity: str
+    threat: str
+    ict_index: str
+    starts: int
+    expected_goals: str
+    expected_assists: str
+    expected_goal_involvements: str
+    expected_goals_conceded: str
+    influence_rank: int
+    influence_rank_type: int
+    creativity_rank: int
+    creativity_rank_type: int
+    threat_rank: int
+    threat_rank_type: int
+    ict_index_rank: int
+    ict_index_rank_type: int
+    corners_and_indirect_freekicks_order: Optional[int]
+    corners_and_indirect_freekicks_text: str
+    direct_freekicks_order: Optional[int]
+    direct_freekicks_text: str
+    penalties_order: Optional[int]
+    penalties_text: str
+    expected_goals_per_90: float
+    saves_per_90: float
+    expected_assists_per_90: float
+    expected_goal_involvements_per_90: float
+    expected_goals_conceded_per_90: float
+    goals_conceded_per_90: float
+    now_cost_rank: int
+    now_cost_rank_type: int
+    form_rank: int
+    form_rank_type: int
+    points_per_game_rank: int
+    points_per_game_rank_type: int
+    selected_rank: int
+    selected_rank_type: int
+    starts_per_90: float
+    clean_sheets_per_90: float
+
+class ElementType(BaseModel):
+    id: int
+    plural_name: str
+    plural_name_short: str
+    singular_name: str
+    singular_name_short: str
+    squad_select: int
+    squad_min_play: int
+    squad_max_play: int
+    squad_min_select: Optional[int]=None
+    squad_max_select: Optional[int]=None
+    ui_shirt_specific: bool
+    sub_positions_locked: List[int]
+    element_count: int
+
+class ElementStat(BaseModel):
+    name: str
+    label: str
+
+class Fixture(BaseModel):
+    team_h: int
+    team_a: int
+    event: Optional[int]
+    id: int
+    team_h_difficulty: int
+    team_a_difficulty: int
+    kickoff_time: str
+    # stats: List[dict]
+    team_a_score: Optional[int] = None
+    team_h_score: Optional[int] = None
+    pulse_id: int
+    finished_provisional: bool
+    minutes: int
+    code: int
+    finished: bool
+    started: bool
+    provisional_start_time: bool
+
+class PlayerHistory(BaseModel):
+    element: int
+    fixture: int
+    opponent_team: int
+    total_points: int
+    was_home: bool
+    kickoff_time: str
+    team_h_score: Optional[int]
+    team_a_score: Optional[int]
+    round: int
+    minutes: int
+    goals_scored: int
+    assists: int
+    clean_sheets: int
+    goals_conceded: int
+    own_goals: int
+    penalties_saved: int
+    penalties_missed: int
+    yellow_cards: int
+    red_cards: int
+    saves: int
+    bonus: int
+    bps: int
+    influence: str
+    creativity: str
+    threat: str
+    ict_index: str
+    value: int
+    transfers_balance: int
+    selected: int
+    transfers_in: int
+    transfers_out: int
+    expected_goal_involvements: float   
+    expected_goals: float
+    expected_assists: float
+    expected_goals_conceded: float
+    starts: int
+
+class PlayerFixture(BaseModel):
+    id: int
+    code: int
+    team_h: int
+    team_h_score: Optional[int]
+    team_a: int
+    team_a_score: Optional[int]
+    event: int
+    finished: bool
+    minutes: int
+    provisional_start_time: bool
+    kickoff_time: str
+    event_name: str
+    is_home: bool
+    difficulty: int
+
+class ManagerPick(BaseModel):
+    element: int
+    position: int
+    multiplier: int
+    is_captain: bool
+    is_vice_captain: bool
+
+class ManagerInfo(BaseModel):
+    id: int
+    joined_time: str
+    started_event: int
+    # events_entered: List
+    favourite_team: int
+    player_first_name: str
+    player_last_name: str
+    player_region_id: int
+    player_region_name: str
+    player_region_iso_code_short: str
+    player_region_iso_code_long: str
+    summary_overall_points: int
+    summary_overall_rank: int
+    summary_event_points: int
+    summary_event_rank: int
+    current_event: int
+    name: str
+    name_change_blocked: bool
+    # leagues: dict
+    last_deadline_bank: int
+    last_deadline_value: int
+    last_deadline_total_transfers: int
+    years_active: int
+
+class StaticData(BaseModel):
+    events: List[Event]
+    teams: List[Team]
+    elements: List[Element]
+    element_types: List[ElementType]
+    element_stats: List[ElementStat]
+
+class ManagerSquad(BaseModel):
+    picks: List[ManagerPick]
